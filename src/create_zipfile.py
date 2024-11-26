938e5b795abcca733438ae900aa50063b79e4bf2
Iron Topi <126910367+IronTopi@users.noreply.github.com>
2024-11-26 20:19:54 +0100

    stuff

--- src/create_zipfile.py
+++ src/create_zipfile.py
@@ -1,15 +1,29 @@
 import gzip
 
 import random
-import string
 from settings import Settings
 from tqdm import tqdm
 
+my_settings = Settings()
+
 
 def generate_random_data(size):
-    chars = string.ascii_letters + string.digits
-    for _ in range(size):
-        yield random.choice(chars)
+    chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+    chunk_size = 1024  # 1 KB
+    for _ in range(0, size, chunk_size):
+        yield "".join(random.choice(chars) for _ in range(chunk_size))
+
+
+def generate_static_data(size):
+    static_string = "a" * 1024  # 1 KB of 'a'
+    chunk_size = 1024  # 1 KB
+
+    loop = 0
+    for _ in range(0, size, chunk_size):
+        loop += 1
+        yield static_string
+
+    # print(loop)
 
 
 def create_gzip_file(file_path, data_size):
@@ -20,20 +34,21 @@ def create_gzip_file(file_path, data_size):
         with tqdm(
             total=data_size, unit="B", unit_scale=True, desc="Creating Gzip File"
         ) as pbar:
+            data_generator = (
+                generate_random_data
+                if my_settings.use_random_data
+                else generate_static_data
+            )
             while total_written < data_size:
                 remaining_size = data_size - total_written
                 current_chunk_size = min(chunk_size, remaining_size)
-                random_data = "".join(
-                    next(generate_random_data(current_chunk_size))
-                    for _ in range(current_chunk_size)
-                )
-                f.write(random_data)
+                data = "".join(data_generator(current_chunk_size))
+                f.write(data)
                 total_written += current_chunk_size
                 pbar.update(current_chunk_size)
 
 
 if __name__ == "__main__":
-    my_settings = Settings()
     file_path = my_settings.zip_file_path
     data_size = my_settings.data_size
     create_gzip_file(file_path, data_size)
--- src/settings.py
+++ src/settings.py
@@ -4,6 +4,7 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     zip_file_path: str = "tmp/random_data.txt.gz"
     data_size: int = 1073741824  # 1 GB in bytes
+    use_random_data: bool = False
 
     class Config:
         env_file = ".env"
